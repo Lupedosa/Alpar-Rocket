f275f30fa18e92e2dd4338aa9a0b61fd14fb18eb
Lupedosa <lupedosa.contato@gmail.com>
2025-02-18 16:21:30 -0300

    .

--- Modulo 1/Dia 2/Calculadora de Impostos v2.0/js/script.js
+++ Modulo 1/Dia 2/Calculadora de Impostos v2.0/js/script.js
@@ -8,13 +8,15 @@ const desconto = Math.random() < 0.5 //chance aleatória. 0.5 = 50/50
 
 
 document.getElementById('calculate').addEventListener('click', function () {
-    const preco = parseFloat(document.getElementById('preco').value);
+    let preco = parseFloat(document.getElementById('preco').value);
     let codigo = document.getElementById('codigo').value;
     const local = document.getElementById('local').value;
 
     const ICMS = Math.floor(Math.random() * 14) + 12; // 14 é a diferença de 25 e 12 (ICM máximo e mínimo) +1, e o +12 é a soma do ICMS mínimo para garantir que o valor será igual ou maior que 12   
 
-    let precoBackup = preco
+    let precoBackup = preco;
+
+    let frete = 0;
 
     if (preco > 0 && local === "SP" || local === "RJ" || local === "Outro"){
 
@@ -30,23 +32,24 @@ document.getElementById('calculate').addEventListener('click', function () {
 
             switch(local){
                 case "SP":
-                    total = total + 10
+                    frete = 10;
+                    total = total + 10;
                     break;
                 case "RJ":
-                    total = total + 20
+                    frete = 20;
+                    total = total + 20;
                     break;
                 case "Outro":
-                    total = total + 30
+                    frete = 30;
+                    total = total + 30;
                     break;
             }
-
+            
             alert("Valor do seu produto: R$ " + preco + "\nFrete: R$ " + frete + "\nICMS: " + ICMS + "%\nTotal: R$ " + total);
 
         }else{
 
             let total = preco + (preco * (ICMS / 100));
-
-            let frete;
 
             switch(local){
                 case "SP":
